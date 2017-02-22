ab76ebb5e11fd733d9a4d984850b5e4755485789
Yogesh Sharma <sharma.yogesh.1234@gmail.com>
2017-02-22 17:17:15 +0530

    Added package component also added route component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { PackageComponent } from './package/package.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+const appRoutes: Routes = [
+    { path: 'packages', component: PackageComponent }
+];
+
+@NgModule( {
+    declarations: [
+        AppComponent,
+        PackageComponent
+    ],
+    imports: [
+        RouterModule.forRoot( appRoutes ),
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
